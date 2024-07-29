4eb7616fff1dc63375ef2577eb94311ae9ea89a1
kaoru <k@warpnine.io>
2024-07-29 21:05:47 +0900

    example: small changes for clarity

--- examples/basic.rs
+++ examples/basic.rs
@@ -3,10 +3,9 @@ use std::fs::read_to_string;
 use markdown_split::split;
 
 fn main() -> anyhow::Result<()> {
-    tracing_subscriber::fmt::init();
-    let markdown_file = read_to_string("tests/fixtures/ch01-01-installation.en.md")?;
+    let markdown = read_to_string("tests/fixtures/ch01-01-installation.en.md")?;
 
-    split(&markdown_file, None)?
+    split(&markdown, None)?
         .iter()
         .enumerate()
         .for_each(|(i, s)| println!("Section {i}\n---------\n{s}\n---------"));
